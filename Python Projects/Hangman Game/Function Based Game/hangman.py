648ff133805c43b42b5f7331ca0159801fac1fa5
Ali Shair <alishair21210@gmail.com>
2024-06-14 23:18:02 -0700

    hangman

---
+++ Python Projects/Hangman Game/Function Based Game/hangman.py
@@ -0,0 +1,117 @@
+import random
+
+def choose_words():
+    Fruits = ['watermelon', 'grapes', 'melon', 'apple', 'banana']
+    Languages = ['python', 'c++', 'c#', 'javascript', 'java']
+    Games = ['pubg', 'minecarft', 'free fire', 'among us']
+
+    Items_list = [Fruits, Languages, Games]
+
+    words = random.choice(Items_list)
+
+    return random.choice(words)
+
+def hangman_display(teris):
+    stage = [
+       """
+         ------
+         |    |
+         |    O
+         |   \|/
+         |    |
+         |   / \\
+         -
+        """,
+       """
+         ------
+         |    |
+         |    O
+         |   \|/
+         |    |
+         |   / 
+         -
+        """,
+       """
+         ------
+         |    |
+         |    O
+         |   \|/
+         |    |
+         |  
+         -
+        """,
+       """
+         ------
+         |    |
+         |    O
+         |   \|
+         |    |
+         |
+         -
+        """,
+        """
+         ------
+         |    |
+         |    O
+         |    |
+         |    |
+         |
+         -
+        """,
+        """
+         ------
+         |    |
+         |    O
+         |
+         |
+         |
+         -
+        """,
+        """
+         ------
+         |    |
+         |   
+         |
+         |
+         |
+         -
+        """
+    ]
+
+    return stage[teris]
+
+def play_hangman():
+    word = choose_words()
+    word_letters = set(word)
+    guessed_letters = set()
+    teris = 6
+
+    print("Welcome to Hangman Game\n")
+
+    while teris > 0 and len(word_letters) > 0:
+        print(hangman_display(teris))
+        print(f"You have only {teris} teris")
+        print("Guess Letters:", ' '.join(sorted(guessed_letters)))
+
+        letters = [letter if letter in guessed_letters else '_' for letter in word]
+        print('Current Word:', ' '.join(letters))
+
+        guess = input("Guess the Letter: ").lower()
+
+        if guess in guessed_letters:
+            print(f"You already guessed {guess} ! Trt Again")
+        elif guess in word_letters:
+            guessed_letters.add(guess)
+            word_letters.remove(guess)
+            print(f"Good Job {guess} added in Word")
+        else:
+            guessed_letters.add(guess)
+            teris -= 1
+            print(f"Sorry, {guess} not in the Word")
+
+    if len(word_letters) == 0:
+        print("Congratulation You Win the Word is", word)
+    else:
+        print("You ran out of Teris, the Word is", word)
+
+play_hangman()
